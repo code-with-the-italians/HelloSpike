de04e7c354f45e5cfb826aa334506137ffd32271
Ivan Morgillo <imorgillo@gmail.com>
2022-03-22 21:10:21 +0100

    Initial commit

---
+++ app/src/main/java/it/codewiththeitalians/hellospike/MainActivity.kt
@@ -0,0 +1,41 @@
+package it.codewiththeitalians.hellospike
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import it.codewiththeitalians.hellospike.ui.theme.HelloSpikeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloSpikeTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = "Hello Spike!",
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.padding(32.dp)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.spike),
+                            contentDescription = "A picture of Spike",
+                            modifier = Modifier.padding(32.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
